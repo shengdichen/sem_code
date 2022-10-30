6fcf0f8ffbb7d00136d9fd1c505e5d4848508b6c
shc <sh@c>
2022-10-30 10:10:47 +0100

    Refactor into tester class in util/test.py

--- src/ours/util/test.py
+++ src/ours/util/test.py
@@ -6,46 +6,48 @@ from src.ours.env.env import MovePoint
 from src.ours.util.helper import Plotter
 
 
-def test_policy(
-    fname,
-    model=None,
-    rm="ERM",
-    shift_x=0,
-    shift_y=0,
-    n_timesteps=2000,
-    deterministic=True,
-):
-    if model is None:
-        model = PPOSB.load(fname)
-    testing_env = MovePoint(2, shift_x=shift_x, shift_y=shift_y)
+class PolicyTester:
+    @staticmethod
+    def test_policy(
+        fname,
+        model=None,
+        rm="ERM",
+        shift_x=0,
+        shift_y=0,
+        n_timesteps=2000,
+        deterministic=True,
+    ):
+        if model is None:
+            model = PPOSB.load(fname)
+        testing_env = MovePoint(2, shift_x=shift_x, shift_y=shift_y)
 
-    obs_list = []
-    obs = testing_env.reset()
-    rew = 0
-    cum_rew = []
+        obs_list = []
+        obs = testing_env.reset()
+        rew = 0
+        cum_rew = []
 
-    for i in range(n_timesteps):
-        action, _states = model.predict(obs, deterministic=deterministic)
-        obs, r, done, info = testing_env.step(action)
-        rew += r
-        if done:
-            obs = testing_env.reset()
-            print(rm + " rewards: ", rew)
-            cum_rew.append(rew)
+        for i in range(n_timesteps):
+            action, _states = model.predict(obs, deterministic=deterministic)
+            obs, r, done, info = testing_env.step(action)
+            rew += r
+            if done:
+                obs = testing_env.reset()
+                print(rm + " rewards: ", rew)
+                cum_rew.append(rew)
 
-            rew_erm = 0
+                rew_erm = 0
 
-        obs_list.append(obs)
+            obs_list.append(obs)
 
-    print(rm + " mean / std cumrew: ", np.mean(cum_rew), np.std(cum_rew))
-    obsa = np.stack(obs_list)
-    x, y, bins = Plotter.get_hist_data(obsa)
+        print(rm + " mean / std cumrew: ", np.mean(cum_rew), np.std(cum_rew))
+        obsa = np.stack(obs_list)
+        x, y, bins = Plotter.get_hist_data(obsa)
 
-    fig = plt.figure()
-    plt.title(rm)
-    # plt.hist2d(x,y,bins)
-    plt.plot(obsa[:, 0], obsa[:, 1], "m-", alpha=0.3)
-    plt.scatter(obsa[:, 2], obsa[:, 3], c="r")
-    plt.axis("equal")
+        fig = plt.figure()
+        plt.title(rm)
+        # plt.hist2d(x,y,bins)
+        plt.plot(obsa[:, 0], obsa[:, 1], "m-", alpha=0.3)
+        plt.scatter(obsa[:, 2], obsa[:, 3], c="r")
+        plt.axis("equal")
 
-    return fig
+        return fig
