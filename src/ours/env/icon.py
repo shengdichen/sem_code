32611d4af1da2a290cc30c7e5d0b79eee73da22e
shc <sh@c>
2022-11-15 17:58:56 +0100

    Add separate class for icon handling

---
+++ src/ours/env/icon.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import cv2
+
+
+class Icon:
+    def __init__(self, path_rel: str, size: tuple[int, int]):
+        self._icon = None
+        self._load(path_rel)
+        self._resize(size)
+
+    @property
+    def icon(self):
+        return self._icon
+
+    def _load(self, path_rel: str):
+        curr_dir = Path(__file__).absolute().parent
+        path_abs = curr_dir / path_rel
+        self._icon = cv2.imread(str(path_abs)) / 255.0
+
+    def _make_square_icon(self):
+        # self._icon = cv2.circle(image, center_coordinates, radius, color, thickness)
+        pass
+
+    def _resize(self, size: tuple[int, int]):
+        self._icon = cv2.resize(self._icon, size)
