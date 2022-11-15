bba85b5a117b6c9e774062ce614b1ae74de58eb8
shc <sh@c>
2022-11-15 20:30:52 +0100

    Relocate the Point()-class to movement.py

---
+++ src/ours/env/movement.py
@@ -0,0 +1,32 @@
+from src.ours.env.icon import Icon
+
+
+class Point(object):
+    def __init__(self, name, x_max, x_min, y_max, y_min, icon_w, icon_h):
+        self.x = 0
+        self.y = 0
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+        self.name = name
+
+        self.icon_w = icon_w
+        self.icon_h = icon_h
+
+    def set_position(self, x, y):
+        self.x = self.clamp(x, self.x_min, self.x_max - self.icon_w)
+        self.y = self.clamp(y, self.y_min, self.y_max - self.icon_h)
+
+    def get_position(self):
+        return (self.x, self.y)
+
+    def move(self, del_x, del_y):
+        self.x += del_x
+        self.y += del_y
+
+        self.x = self.clamp(self.x, self.x_min, self.x_max - self.icon_w)
+        self.y = self.clamp(self.y, self.y_min, self.y_max - self.icon_h)
+
+    def clamp(self, n, minn, maxn):
+        return max(min(maxn, n), minn)
