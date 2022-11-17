e439c01173ae282a37e9beb9ee5e8c7cc4e43d8c
shc <sh@c>
2022-11-17 08:05:30 +0100

    Remove legacy Agent and Target classes

--- src/ours/env/component.py
+++ src/ours/env/component.py
@@ -46,29 +46,3 @@ class PointFactory:
             "star.png",
             (4, 4),
         )
-
-
-class Agent(Point):
-    def __init__(self, name, x_max, x_min, y_max, y_min):
-        self.name = name
-
-        self.icon_w, self.icon_h = 5, 5
-
-        super(Agent, self).__init__(
-            x_max, x_min, y_max, y_min, self.icon_w, self.icon_h
-        )
-
-        self.icon = Icon("agent.png", (self.icon_w, self.icon_h)).icon
-
-
-class Target(Point):
-    def __init__(self, name, x_max, x_min, y_max, y_min):
-        self.name = name
-
-        self.icon_w, self.icon_h = 4, 4
-
-        super(Target, self).__init__(
-            x_max, x_min, y_max, y_min, self.icon_w, self.icon_h
-        )
-
-        self.icon = Icon("star.png", (self.icon_w, self.icon_h)).icon
