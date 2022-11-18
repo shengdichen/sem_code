f641d13184ceb3aebb10cac2d769ec57f7fceff0
shc <sh@c>
2022-11-18 04:52:03 +0100

    Use env-factory for training expert

--- src/ours/eval/expert.py
+++ src/ours/eval/expert.py
@@ -1,4 +1,4 @@
-from src.ours.env.env import MovePoint
+from src.ours.env.creation import PointEnvFactory
 from src.ours.eval.param import TrainingParam
 from src.ours.util.helper import Plotter
 from src.ours.util.train import TrainerExpert
@@ -14,14 +14,14 @@ class ClientTrainerExpert:
         # Train experts with different shifts representing their waypoint preferences
         """
 
-        self._train(2, 0, 0, "exp_0_0")
-        self._train(2, 0, 50, "exp_0_50")
-        self._train(2, 50, 0, "exp_50_0")
+        self._train({"n_targets": 2, "shift_x": 0, "shift_y": 0}, "exp_0_0")
+        self._train({"n_targets": 2, "shift_x": 0, "shift_y": 50}, "exp_0_50")
+        self._train({"n_targets": 2, "shift_x": 50, "shift_y": 0}, "exp_50_0")
 
         self._plot()
 
-    def _train(self, n_targets, shift_x, shift_y, fname):
-        env = MovePoint(n_targets, shift_x, shift_y)
+    def _train(self, env_config: dict[str:int], fname):
+        env = PointEnvFactory(env_config).create()
         trainer = TrainerExpert(self._training_param, env)
         trainer.train(self._n_timesteps, n_targets, shift_x, shift_y, fname)
 
