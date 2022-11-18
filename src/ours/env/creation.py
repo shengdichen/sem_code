86e639608cd2d3cf6b98d446e90bba2fa3f58e50
shc <sh@c>
2022-11-18 04:47:38 +0100

    Add implem'n for our point-env

--- src/ours/env/creation.py
+++ src/ours/env/creation.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
 
+from src.ours.env.env import MovePoint
+
 
 class EnvFactory(ABC):
     @abstractmethod
     def create(self):
         pass
+
+
+class PointEnvFactory(EnvFactory):
+    def __init__(self, env_config: dict[str:int]):
+        self._env_config = env_config
+
+    def create(self):
+        return MovePoint(**self._env_config)
