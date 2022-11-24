f3c5492bde05d550ff155c7a809c5a7864d3d554
shc <sh@c>
2022-11-24 19:40:10 +0100

    Create separate generator for expert-traj

--- src/ours/util/helper.py
+++ src/ours/util/helper.py
@@ -274,6 +274,50 @@ class ExpertSaveLoad:
         return np.load(self._path)
 
 
+class ExpertTrajGenerator:
+    def __init__(
+        self,
+        env_model: tuple[Env, Any],
+        expert_manager_param=ExpertManagerParam(),
+    ):
+        self._env, self._model = env_model
+        self._expert_manager_param = expert_manager_param
+
+    def get_expert_traj(self):
+        num_steps = 0
+        expert_traj = []
+
+        for i_episode in count():
+            ob = self._env.reset()
+            done = False
+            total_reward = 0
+            episode_traj = []
+
+            while not done:
+                ac, _states = self._model.predict(
+                    ob, deterministic=self._expert_manager_param.deterministic
+                )
+                next_ob, reward, done, _ = self._env.step(ac)
+
+                ob = next_ob
+                total_reward += reward
+                stacked_vec = np.hstack([np.squeeze(ob), np.squeeze(ac), reward, done])
+                expert_traj.append(stacked_vec)
+                episode_traj.append(stacked_vec)
+                num_steps += 1
+                if self._expert_manager_param.render:
+                    self._env.render()
+
+            print("Episode reward: ", total_reward)
+
+            if i_episode > self._expert_manager_param.nr_trajectories:
+                break
+
+        self._env.close()
+
+        return np.stack(expert_traj)
+
+
 class Plotter:
     def __init__(self):
         pass  # intentionally left empty
