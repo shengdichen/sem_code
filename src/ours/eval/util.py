fca8d2047c8f5baced5988360775021e3ae05f97
shc <sh@c>
2022-11-25 23:29:48 +0100

    Create separate Sb3-Manager

--- src/ours/eval/util.py
+++ src/ours/eval/util.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 from stable_baselines3.common.base_class import BaseAlgorithm
 
+from src.ours.eval.param import CommonParam
+from src.ours.util.pathprovider import Sb3SaveLoadPathGenerator
+
 
 class Saver:
     def __init__(self, model: BaseAlgorithm, savepath_rel: Path):
@@ -10,3 +13,14 @@ class Saver:
 
     def save_model(self):
         self._model.save(self._savepath_rel)
+
+
+class SaverManager:
+    def __init__(self, model: BaseAlgorithm, training_param: CommonParam):
+        self._model = model
+        self._path_generator = Sb3SaveLoadPathGenerator(training_param)
+
+    def save(self, env_identifier: str):
+        path_saveload = self._path_generator.get_path(env_identifier)
+        saver = Saver(self._model, path_saveload)
+        saver.save_model()
