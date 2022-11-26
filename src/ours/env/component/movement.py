4bea04f31a7d6f289fa234bac43d2f934be57846
shc <sh@c>
2022-11-26 03:34:53 +0100

    Use tuple-init for c'tor 2D-movement

--- src/ours/env/component/movement.py
+++ src/ours/env/component/movement.py
@@ -1,9 +1,7 @@
 class MovementTwoDim:
     def __init__(self, x_max_with_icon, x_min, y_max_with_icon, y_min):
-        self.x_min = x_min
-        self.y_min = y_min
-        self.x_max_with_icon = x_max_with_icon
-        self.y_max_with_icon = y_max_with_icon
+        self.x_min, self.x_max_with_icon = x_min, x_max_with_icon
+        self.y_min, self.y_max_with_icon = y_min, y_max_with_icon
 
         self.x_movement = MovementOneDim((self.x_min, self.x_max_with_icon))
         self.y_movement = MovementOneDim((self.y_min, self.y_max_with_icon))
