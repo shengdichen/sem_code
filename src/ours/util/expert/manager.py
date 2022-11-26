26ac4fb4a6ca97998fc918b9fbf246976ba4b5a1
shc <sh@c>
2022-11-26 19:32:31 +0100

    Relocate expert-util to separate package

--- src/ours/util/expert/manager.py
+++ src/ours/util/expert/manager.py
@@ -5,8 +5,8 @@ from gym import Env
 
 from src.ours.env.creation import PointEnvIdentifierGenerator
 from src.ours.eval.param import CommonParam
-from src.ours.util.expert.saveload import ExpertSaveLoad
-from src.ours.util.expert.trajectory import (
+from src.ours.util.expert.util.saveload import ExpertSaveLoad
+from src.ours.util.expert.util.trajectory import (
     TrajectoryGeneratorConfig,
     TrajectoryGenerator,
 )
