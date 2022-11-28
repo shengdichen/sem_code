e2b6c4ff699b7c7df6393a7320e53c5b66309732
shc <sh@c>
2022-11-28 23:50:29 +0100

    Add class for 3 default pointenv configs

--- src/ours/env/creation.py
+++ src/ours/env/creation.py
@@ -17,6 +17,19 @@ class PointEnvFactory(EnvFactory):
         return MovePoint(**self._env_config)
 
 
+class PointEnvConfigFactory:
+    def __init__(self):
+        self._env_configs = [
+            {"n_targets": 2, "shift_x": 0, "shift_y": 0},
+            {"n_targets": 2, "shift_x": 0, "shift_y": 50},
+            {"n_targets": 2, "shift_x": 50, "shift_y": 0},
+        ]
+
+    @property
+    def env_configs(self):
+        return self._env_configs
+
+
 class PointEnvIdentifierGenerator:
     def __init__(self, env_config: dict[str:int]):
         self._env_config = env_config
