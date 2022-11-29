86663353f1bc7a477551e836ee0a917be3bdb3d1
shc <sh@c>
2022-11-29 04:21:05 +0100

    Add pointenv client for 1 single instance

--- src/ours/eval/pointenv/expert.py
+++ src/ours/eval/pointenv/expert.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from src.ours.env.creation import (
     PointEnvFactory,
     PointEnvIdentifierGenerator,
@@ -9,6 +11,31 @@ from src.ours.util.expert.client import ClientExpert
 from src.ours.util.expert.manager import ExpertManager
 from src.ours.util.expert.sb3.util.train import TrainerExpert
 from src.ours.util.common.helper import Plotter
+
+
+class PointEnvExpertSingle:
+    def __init__(self, env_config: dict[str:int]):
+        self._training_param = ExpertParam()
+
+        env = PointEnvFactory(env_config).create()
+        trainer = TrainerExpert(env, self._training_param)
+        env_identifier = PointEnvIdentifierGenerator().from_env(env)
+
+        self._expert_client = ClientExpert(
+            trainer,
+            (
+                Sb3Manager(trainer.model, self._training_param),
+                ExpertManager((env, trainer.model), self._training_param),
+            ),
+            env_identifier,
+        )
+
+    def train_and_save(self) -> None:
+        self._expert_client.train()
+        self._expert_client.save()
+
+    def load(self) -> np.ndarray:
+        return self._expert_client.load()
 
 
 class PointEnvExpert:
