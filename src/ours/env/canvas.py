11f0a3651c9f69852c5853429afb0f11a0162878
shc <sh@c>
2022-12-01 08:18:10 +0100

    Add subclass-visualizer for canvas

--- src/ours/env/canvas.py
+++ src/ours/env/canvas.py
@@ -14,6 +14,33 @@ class VisuaizerBase(ABC):
         pass
 
 
+class CanvasVisualizer(VisuaizerBase):
+    def __init__(self, canvas_shape: tuple[int, int]):
+        super().__init__(canvas_shape)
+
+        self._canvas = np.ones(self._colormap_shape)
+
+    @property
+    def canvas(self):
+        return self._canvas
+
+    def register(self, points: list[NamedPointWithIcon]) -> None:
+        self._canvas = np.ones(self._colormap_shape)
+
+        for point in points:
+            self._canvas[
+                point.movement.y : point.movement.y + point.y_icon,
+                point.movement.x : point.movement.x + point.x_icon,
+            ] = point.icon
+
+        # text = 'Time Left: {} | Rewards: {}'.format(self.time, self.ep_return)
+
+        # Put the info on canvas
+        # self.canvas = cv2.putText(
+        #     self.canvas, text, (10, 20), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA
+        # )
+
+
 class Visualizer:
     def __init__(self, canvas_shape: tuple[int, int]):
         self._colormap_shape = canvas_shape[0], canvas_shape[1], 3
