faacaac1318c0831004c83b1f20d4a4ed2e78cc9
shc <sh@c>
2022-12-04 22:22:23 +0100

    Reorder member methods

--- src/ours/env/env.py
+++ src/ours/env/env.py
@@ -13,6 +13,13 @@ class EpisodeLengthTimer:
     def __init__(self, max_episode_length=1000):
         self._max_episode_length, self._curr_episode_length = max_episode_length, 0
 
+    def reset(self) -> None:
+        self._curr_episode_length = 0
+
+    def advance(self) -> bool:
+        self._tick()
+        return self._has_elapsed()
+
     def _tick(self) -> None:
         self._curr_episode_length += 1
 
@@ -20,13 +27,6 @@ class EpisodeLengthTimer:
         if self._curr_episode_length > self._max_episode_length:
             return True
         return False
-
-    def advance(self) -> bool:
-        self._tick()
-        return self._has_elapsed()
-
-    def reset(self) -> None:
-        self._curr_episode_length = 0
 
 
 class MovePoint(Env):
