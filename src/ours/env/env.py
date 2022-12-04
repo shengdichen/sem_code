78341a9632b2c56fb9e036bd700fe15c2fdb86ea
shc <sh@c>
2022-12-04 22:21:40 +0100

    Add type-hinting

--- src/ours/env/env.py
+++ src/ours/env/env.py
@@ -13,19 +13,19 @@ class EpisodeLengthTimer:
     def __init__(self, max_episode_length=1000):
         self._max_episode_length, self._curr_episode_length = max_episode_length, 0
 
-    def _tick(self):
+    def _tick(self) -> None:
         self._curr_episode_length += 1
 
-    def _has_elapsed(self):
+    def _has_elapsed(self) -> bool:
         if self._curr_episode_length > self._max_episode_length:
             return True
         return False
 
-    def advance(self):
+    def advance(self) -> bool:
         self._tick()
         return self._has_elapsed()
 
-    def reset(self):
+    def reset(self) -> None:
         self._curr_episode_length = 0
 
 
