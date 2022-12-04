dd9c10b55ffa493abd823a967e78017db0a63876
shc <sh@c>
2022-12-04 22:03:25 +0100

    Make x&y movement-members private

--- src/ours/env/component/point/movement.py
+++ src/ours/env/component/point/movement.py
@@ -3,25 +3,25 @@ import numpy as np
 
 class MovementTwoDim:
     def __init__(self, x_min_max_with_icon, y_min_max_with_icon):
-        self.x_movement = MovementOneDim(x_min_max_with_icon)
-        self.y_movement = MovementOneDim(y_min_max_with_icon)
+        self._x_movement = MovementOneDim(x_min_max_with_icon)
+        self._y_movement = MovementOneDim(y_min_max_with_icon)
 
     def set_position(self, pos_desired: tuple[float, float]) -> None:
         pos_desired_x, pos_desired_y = pos_desired
-        self.x_movement.set(pos_desired_x)
-        self.y_movement.set(pos_desired_y)
+        self._x_movement.set(pos_desired_x)
+        self._y_movement.set(pos_desired_y)
 
     def get_position(self) -> tuple[float, float]:
-        return self.x_movement.pos, self.y_movement.pos
+        return self._x_movement.pos, self._y_movement.pos
 
     def shift(self, shift_by_x: float, shift_by_y: float) -> None:
-        self.x_movement.shift(shift_by_x)
-        self.y_movement.shift(shift_by_y)
+        self._x_movement.shift(shift_by_x)
+        self._y_movement.shift(shift_by_y)
 
     def distance_l2(self, that: "MovementTwoDim") -> float:
         return np.sqrt(
-            self.x_movement.diff_squared(that.x_movement)
-            + self.y_movement.diff_squared(that.y_movement)
+            self._x_movement.diff_squared(that._x_movement)
+            + self._y_movement.diff_squared(that._y_movement)
         )
 
 
