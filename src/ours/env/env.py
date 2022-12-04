2d7c703d64990962e0790ef9cd53c2c7c326904f
shc <sh@c>
2022-12-04 22:25:00 +0100

    Relocate Timer to separate module

--- src/ours/env/env.py
+++ src/ours/env/env.py
@@ -7,26 +7,7 @@ from src.ours.env.component.visualizer import (
 from src.ours.env.component.field import Field
 from src.ours.env.util.space import SpacesGenerator, ActionConverter
 from src.ours.env.util.renderer import PointEnvRendererHuman, PointEnvRendererRgb
-
-
-class EpisodeLengthTimer:
-    def __init__(self, max_episode_length=1000):
-        self._max_episode_length, self._curr_episode_length = max_episode_length, 0
-
-    def reset(self) -> None:
-        self._curr_episode_length = 0
-
-    def advance(self) -> bool:
-        self._tick()
-        return self._has_elapsed()
-
-    def _tick(self) -> None:
-        self._curr_episode_length += 1
-
-    def _has_elapsed(self) -> bool:
-        if self._curr_episode_length > self._max_episode_length:
-            return True
-        return False
+from src.ours.env.util.time import EpisodeLengthTimer
 
 
 class MovePoint(Env):
