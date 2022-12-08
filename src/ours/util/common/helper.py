9846b670b01f16f5fa6e88d140b494ab487cb27b
shc <sh@c>
2022-12-08 20:53:44 +0100

    Relocate traj-inspector to separate module

--- src/ours/util/common/helper.py
+++ src/ours/util/common/helper.py
@@ -6,6 +6,8 @@ import torch
 from stable_baselines3.common.callbacks import BaseCallback
 from tqdm import tqdm
 
+from src.ours.util.expert.analyzer.plotter import TrajectoryInspector
+
 
 class RewardPlotter:
     @staticmethod
@@ -171,56 +173,6 @@ class TrajectoriesPlotter:
             TrajectoryInspector(trajectory).plot_agent_and_target(axs, plot_hist)
 
         plt.show()
-
-
-class TrajectoryInspector:
-    def __init__(self, trajectory: np.ndarray):
-        self._trajectory_interpreter = TrajectoryInterpreter(trajectory)
-
-        self._bins_hist = self._make_bins_hist()
-
-    @staticmethod
-    def _make_bins_hist(nr=40, canvas_size=200):
-        x_bins = np.linspace(0, canvas_size, nr)
-        y_bins = np.linspace(0, canvas_size, nr)
-
-        return x_bins, y_bins
-
-    def plot_agent_and_target(
-        self, axs: tuple[plt.Axes, plt.Axes], plot_agent_with_hist: bool
-    ) -> None:
-        if plot_agent_with_hist:
-            self._plot_agent_hist(axs[0])
-        else:
-            self._plot_agent_direct(axs[0])
-
-        self._plot_target(axs[1])
-
-    def _plot_agent_hist(self, ax: plt.Axes) -> None:
-        agent_pos_x, agent_pos_y = self._trajectory_interpreter.agent_pos
-        ax.hist2d(agent_pos_x, agent_pos_y, bins=self._bins_hist)
-
-    def _plot_agent_direct(self, ax: plt.Axes) -> None:
-        agent_pos_x, agent_pos_y = self._trajectory_interpreter.agent_pos
-        ax.plot(agent_pos_x, agent_pos_y, "m-", alpha=0.3)
-
-    def _plot_target(self, ax: plt.Axes) -> None:
-        target_pos_x, target_pos_y = self._trajectory_interpreter.target_pos
-        ax.scatter(target_pos_x, target_pos_y, c="r")
-
-    def _plot_hist_and_action(self) -> None:
-        __, axs = plt.subplots(1, 2)
-
-        self._plot_agent_hist(axs[0])
-        self._plot_action(axs[1])
-
-        plt.show()
-
-    def _plot_action(self, ax: plt.Axes) -> None:
-        ax.hist(self._trajectory_interpreter.action)
-
-    def display_stats(self) -> None:
-        self._trajectory_interpreter.display_stats()
 
 
 class TrajectoryInterpreter:
