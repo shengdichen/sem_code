eea9cefb90b370dc12bcebfbfc239a833a796849
shc <sh@c>
2022-12-08 21:06:14 +0100

    Relocate interpreter to separate module

--- src/ours/util/expert/analyzer/plotter.py
+++ src/ours/util/expert/analyzer/plotter.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from src.ours.util.common.helper import TrajectoryInterpreter
+from src.ours.util.expert.analyzer.stats import TrajectoryInterpreter
 
 
 class TrajectoryInspector:
