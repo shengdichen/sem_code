9303b77d15214dd76a73d527ae612501ea63e6ae
shc <sh@c>
2022-12-10 05:48:32 +0100

    Relocate stats&plotter of trajs in c'tor

--- src/ours/util/expert/analyzer/general.py
+++ src/ours/util/expert/analyzer/general.py
@@ -9,8 +9,16 @@ from src.ours.util.expert.analyzer.stats import TrajectoryStats
 class TrajectoriesAnalyzer:
     def __init__(self, trajectories: list[np.ndarray]):
         self._trajectories = trajectories
+        self._trajectories_stats = [
+            TrajectoryStats(trajectory) for trajectory in self._trajectories
+        ]
 
         self._figures = self._get_configured_figures()
+
+        self._trajectories_plotter = [
+            TrajectoryPlotter(trajectory, subfigure)
+            for trajectory, subfigure in zip(self._trajectories, self._figures)
+        ]
 
     def _get_configured_figures(self) -> list[matplotlib.figure.SubFigure]:
         figure = plt.figure(figsize=[15, 5])
@@ -23,12 +31,10 @@ class TrajectoriesAnalyzer:
         plt.show()
 
     def analyze(self, plot_agent_as_hist: bool = True) -> None:
-        for trajectory in self._trajectories:
-            TrajectoryStats(trajectory).display_stats()
+        for trajectory_stats in self._trajectories_stats:
+            trajectory_stats.display_stats()
 
-        for trajectory, subfigure in zip(self._trajectories, self._figures):
-            TrajectoryPlotter(trajectory, subfigure).plot_agent_and_target(
-                plot_agent_as_hist
-            )
+        for trajectory_plotter in self._trajectories_plotter:
+            trajectory_plotter.plot_agent_and_target(plot_agent_as_hist)
 
         self._show_figures()
