1158bd8c1f8bb847709a3a839ff87d3ab3113b43
shc <sh@c>
2022-12-12 20:30:01 +0100

    Add Loading class for sb3

--- src/ours/util/expert/sb3/util/saveload.py
+++ src/ours/util/expert/sb3/util/saveload.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import gym
 from stable_baselines3.common.base_class import BaseAlgorithm
 
 
@@ -10,3 +11,12 @@ class Sb3Saver:
 
     def save_model(self):
         self._model.save(self._savepath_rel)
+
+
+class Sb3Loader:
+    def __init__(self, alg: BaseAlgorithm, savepath_rel: Path):
+        self._alg = alg
+        self._savepath_rel = savepath_rel
+
+    def load_model(self, env: gym.Env = None) -> BaseAlgorithm:
+        return self._alg.load(self._savepath_rel, env)
