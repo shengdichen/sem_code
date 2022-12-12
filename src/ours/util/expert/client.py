6fae5aa6d5760548d6b7b140e08d21297c279432
shc <sh@c>
2022-12-12 21:34:14 +0100

    Remove sb3-trainer from c'tor and as member

--- src/ours/util/expert/client.py
+++ src/ours/util/expert/client.py
@@ -2,22 +2,19 @@ import numpy as np
 
 from src.ours.util.expert.sb3.manager import Sb3Manager
 from src.ours.util.expert.trajectory.manager import TrajectoryManager
-from src.ours.util.expert.sb3.util.train import Sb3Trainer
 
 
 class ClientExpert:
     def __init__(
         self,
-        trainer: Sb3Trainer,
         managers: tuple[Sb3Manager, TrajectoryManager],
         env_identifier: str,
     ):
-        self._trainer = trainer
         self._sb3_manager, self._trajectory_manager = managers
         self._env_identifier = env_identifier
 
     def train(self) -> None:
-        self._trainer.train()
+        self._sb3_manager.train()
 
     def save(self) -> None:
         self._sb3_manager.save(self._env_identifier)
