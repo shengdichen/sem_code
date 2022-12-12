f0627b9f101e5fd7499137a2a113267783a1c28a
shc <sh@c>
2022-12-12 21:24:15 +0100

    Init sb3-manager with env-aware trainer

--- src/ours/util/expert/sb3/manager.py
+++ src/ours/util/expert/sb3/manager.py
@@ -4,18 +4,22 @@ from stable_baselines3.common.base_class import BaseAlgorithm
 from src.ours.util.common.param import CommonParam
 from src.ours.util.common.pathprovider import Sb3SaveLoadPathGenerator
 from src.ours.util.expert.sb3.util.saveload import Sb3Saver, Sb3Loader
+from src.ours.util.expert.sb3.util.train import TrainerExpert
 
 
 class Sb3Manager:
-    def __init__(self, model: BaseAlgorithm, training_param: CommonParam):
-        self._model = model
+    def __init__(self, env: gym.Env, training_param: CommonParam):
+        self._trainer = TrainerExpert(env, training_param)
         self._path_generator = Sb3SaveLoadPathGenerator(training_param)
+
+    def train(self) -> None:
+        self._trainer.train()
 
     def save(self, env_identifier: str):
         path_saveload = self._path_generator.get_path(env_identifier)
-        saver = Sb3Saver(self._model, path_saveload)
+        saver = Sb3Saver(self._trainer.model, path_saveload)
         saver.save_model()
 
     def load(self, env_identifier: str, env: gym.Env = None) -> BaseAlgorithm:
         path_saveload = self._path_generator.get_path(env_identifier)
-        return Sb3Loader(self._model, path_saveload).load_model(env)
+        return Sb3Loader(self._trainer.model, path_saveload).load_model(env)
