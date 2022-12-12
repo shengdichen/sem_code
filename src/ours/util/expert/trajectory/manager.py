14e4d594c55f49a02c888ffefdae790a30c6f175
shc <sh@c>
2022-12-12 23:18:24 +0100

    Make saveload-path class-member for traj-manager

--- src/ours/util/expert/trajectory/manager.py
+++ src/ours/util/expert/trajectory/manager.py
@@ -5,6 +5,7 @@ from gym import Env
 
 from src.ours.util.common.param import CommonParam
 from src.ours.util.common.pathprovider import TrajectorySaveLoadPathGenerator
+from src.ours.util.expert.trajectory.analyzer.general import TrajectoriesAnalyzer
 from src.ours.util.expert.trajectory.util.saveload import TrajectorySaveLoad
 from src.ours.util.expert.trajectory.util.generator import (
     TrajectoryGeneratorConfig,
@@ -19,19 +20,19 @@ class TrajectoryManager:
         model_and_training_param: tuple[Any, CommonParam],
         trajectory_generator_config=TrajectoryGeneratorConfig(),
     ):
-        env, self._env_identifier = env_and_identifier
+        env, env_identifier = env_and_identifier
         model, training_param = model_and_training_param
         self._trajectory_generator = TrajectoryGenerator(
             (env, model), trajectory_generator_config
         )
-        self._path_generator = TrajectorySaveLoadPathGenerator(training_param)
+        self._path_saveload = TrajectorySaveLoadPathGenerator(training_param).get_path(
+            env_identifier
+        )
 
     def save_trajectory(self) -> None:
         trajectory = self._trajectory_generator.get_trajectory()
-        path_saveload = self._path_generator.get_path(self._env_identifier)
 
-        TrajectorySaveLoad(path_saveload).save(trajectory)
+        TrajectorySaveLoad(self._path_saveload).save(trajectory)
 
     def load_trajectory(self) -> np.ndarray:
-        path_saveload = self._path_generator.get_path(self._env_identifier)
-        return TrajectorySaveLoad(path_saveload).load()
+        return TrajectorySaveLoad(self._path_saveload).load()
