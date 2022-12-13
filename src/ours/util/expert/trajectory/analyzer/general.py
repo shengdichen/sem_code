fd6270f106143cbec0dc60ac553c0d460ad0d5fd
shc <sh@c>
2022-12-13 04:37:31 +0100

    Relocate MlpUtil to separate module

--- src/ours/util/expert/trajectory/analyzer/general.py
+++ src/ours/util/expert/trajectory/analyzer/general.py
@@ -1,31 +1,9 @@
 import matplotlib
 import numpy as np
-from matplotlib import pyplot as plt
 
 from src.ours.util.expert.trajectory.analyzer.plotter import TrajectoryPlotter
 from src.ours.util.expert.trajectory.analyzer.stats import TrajectoryStats
-
-
-class MplUtil:
-    def __init__(self, n_trajectories: int):
-        self._n_trajectories = n_trajectories
-
-    def get_parallel_figures(
-        self,
-    ) -> list[matplotlib.figure.Figure] | list[matplotlib.figure.SubFigure]:
-        figure = plt.figure(figsize=[15, 5])
-
-        if self._n_trajectories == 1:
-            return [figure]
-        else:
-            return figure.subfigures(1, self._n_trajectories)
-
-    def get_separate_figures(self) -> list[matplotlib.figure.Figure]:
-        return [plt.figure(figsize=[15, 5]) for __ in range(self._n_trajectories)]
-
-    @staticmethod
-    def show_figures() -> None:
-        plt.show()
+from src.ours.util.expert.trajectory.analyzer.util import MplUtil
 
 
 class TrajectoriesAnalyzerBase:
