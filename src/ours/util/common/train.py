79cd0639414e7c086d3e2565c43594a523be4181
shc <sh@c>
2022-12-15 06:29:22 +0100

    Remove training-param in c'tor of base-trainer

--- src/ours/util/common/train.py
+++ src/ours/util/common/train.py
@@ -1,9 +1,3 @@
-from src.ours.util.common.param import CommonParam
-
-
 class Trainer:
-    def __init__(self, training_param: CommonParam):
-        self._training_param = training_param
-
     def train(self, **kwargs):
         pass
