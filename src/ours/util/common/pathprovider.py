00b9c3d31c8139f5c1d956b561bf23ddc25ab4ec
shc <sh@c>
2023-01-03 12:28:38 +0100

    Add get'r for plot-path

--- src/ours/util/common/pathprovider.py
+++ src/ours/util/common/pathprovider.py
@@ -45,6 +45,9 @@ class PwilSaveLoadPathGenerator:
     def get_trajectory_path(self, env_identifier: str) -> Path:
         return self.get_path(self._training_param.demo_dir, env_identifier)
 
+    def get_plot_path(self, env_identifier: str) -> Path:
+        return self.get_path(self._training_param.plot_dir, env_identifier)
+
     def get_path(self, raw_dir: str, env_identifier: str) -> Path:
         n_demos = self._training_param.pwil_training_param["n_demos"]
         subsampling = self._training_param.pwil_training_param["subsampling"]
