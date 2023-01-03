c0e2f6cf6b20c3d3b35d865ef05f08a2cd0a81b2
shc <sh@c>
2023-01-03 18:35:01 +0100

    Use plot's saveload path

--- src/ours/util/pwil/train.py
+++ src/ours/util/pwil/train.py
@@ -119,7 +119,7 @@ class RewardPlotManager:
         self._env_pwil_rewarded, env_identifier = env_pwil_rewarded_and_identifier
 
         self._reward_plot = self._make_reward_plot()
-        self._path_saveload = PwilSaveLoadPathGenerator(training_param).get_path(
+        self._path_saveload = PwilSaveLoadPathGenerator(training_param).get_plot_path(
             env_identifier
         )
 
