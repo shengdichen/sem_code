dea922698f94fffe70fad5570801c6c0b35ae22e
shc <sh@c>
2023-01-05 20:24:44 +0100

    Moe rewardplot-manager to rewardplot/manager

---
+++ src/ours/util/pwil/rewardplot/manager.py
@@ -0,0 +1,50 @@
+import numpy as np
+from PIL import Image
+from gym import Env
+from matplotlib import pyplot as plt
+
+from src.ours.util.common.helper import RewardPlotter
+from src.ours.util.common.param import PwilParam
+from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
+
+
+class RewardPlotManager:
+    def __init__(
+        self,
+        training_param: PwilParam,
+        env_pwil_rewarded_and_identifier: tuple[Env, str],
+    ):
+        self._env_pwil_rewarded, env_identifier = env_pwil_rewarded_and_identifier
+
+        self._reward_plot = self._make_reward_plot()
+        self._path_saveload = PwilSaveLoadPathGenerator(training_param).get_plot_path(
+            env_identifier
+        )
+
+    @property
+    def reward_plot(self) -> np.ndarray:
+        return self._reward_plot
+
+    def _make_reward_plot(self) -> np.ndarray:
+        plot = RewardPlotter.plot_reward(
+            discriminator=None, env=self._env_pwil_rewarded
+        )
+
+        return plot
+
+    def save_reward_plot(self, save_np: bool = True) -> None:
+        im = Image.fromarray(self._reward_plot)
+
+        save_path = str(self._path_saveload) + ".png"
+        im.save(save_path)
+
+        if save_np:
+            self.save_reward_plot_np()
+
+    def show_reward_plot(self) -> None:
+        ax = plt.figure().subplots()
+        ax.imshow(self._reward_plot)
+        plt.show()
+
+    def save_reward_plot_np(self) -> None:
+        np.save(str(self._path_saveload), self._reward_plot)
--- src/ours/util/pwil/train.py
+++ src/ours/util/pwil/train.py
@@ -1,13 +1,10 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 from gym import Env
 from stable_baselines3.common.base_class import BaseAlgorithm
 
-from src.ours.util.common.helper import RewardPlotter
 from src.ours.util.common.param import PwilParam
-from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
 from src.ours.util.expert.trajectory.manager import TrajectoryManager
+from src.ours.util.pwil.rewardplot.manager import RewardPlotManager
 from src.ours.util.pwil.sb3.manager import Sb3PwilManager
 from src.upstream.env_utils import PWILReward
 
@@ -36,48 +33,6 @@ class PwilEnvFactory:
         )
 
         return env_pwil_rewarded
-
-
-class RewardPlotManager:
-    def __init__(
-        self,
-        training_param: PwilParam,
-        env_pwil_rewarded_and_identifier: tuple[Env, str],
-    ):
-        self._env_pwil_rewarded, env_identifier = env_pwil_rewarded_and_identifier
-
-        self._reward_plot = self._make_reward_plot()
-        self._path_saveload = PwilSaveLoadPathGenerator(training_param).get_plot_path(
-            env_identifier
-        )
-
-    @property
-    def reward_plot(self) -> np.ndarray:
-        return self._reward_plot
-
-    def _make_reward_plot(self) -> np.ndarray:
-        plot = RewardPlotter.plot_reward(
-            discriminator=None, env=self._env_pwil_rewarded
-        )
-
-        return plot
-
-    def save_reward_plot(self, save_np: bool = True) -> None:
-        im = Image.fromarray(self._reward_plot)
-
-        save_path = str(self._path_saveload) + ".png"
-        im.save(save_path)
-
-        if save_np:
-            self.save_reward_plot_np()
-
-    def show_reward_plot(self) -> None:
-        ax = plt.figure().subplots()
-        ax.imshow(self._reward_plot)
-        plt.show()
-
-    def save_reward_plot_np(self) -> None:
-        np.save(str(self._path_saveload), self._reward_plot)
 
 
 class PwilManager:
