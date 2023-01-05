2df8bc8aca3d6e4db3cda9ef6430e660794b39ca
shc <sh@c>
2023-01-05 18:08:59 +0100

    Add separate class for numpy-saveload

--- src/ours/util/expert/trajectory/util/saveload.py
+++ src/ours/util/expert/trajectory/util/saveload.py
@@ -3,9 +3,21 @@ from pathlib import Path
 import numpy as np
 
 
+class NumpySaveLoad:
+    def __init__(self, path: Path):
+        self._path = str(path)
+
+    def save(self, target: np.ndarray) -> None:
+        np.save(self._path, target)
+
+    def load(self) -> np.ndarray:
+        return np.load(self._path)
+
+
 class TrajectorySaveLoad:
     def __init__(self, path: Path):
         self._path = str(path)
+        self._saveloader = NumpySaveLoad(path)
 
     def save(self, target: np.ndarray) -> None:
         np.save(self._path, target)
