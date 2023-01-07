33e7589c79cb36320066fc963a57a2acd604c16a
shc <sh@c>
2023-01-07 15:32:08 +0100

    Use 1-traj appender for get'g all traj

--- src/ours/util/expert/trajectory/util/generator.py
+++ src/ours/util/expert/trajectory/util/generator.py
@@ -38,30 +38,7 @@ class TrajectoryGenerator:
 
     def get_trajectory(self) -> np.ndarray:
         for __ in range(self._trajectory_generator_config.nr_trajectories):
-            obs_curr = self._env.reset()
-            done = False
-            reward_curr_episode, snapshots_curr_episode = 0, []
-
-            while not done:
-                action, __ = self._model.predict(
-                    obs_curr,
-                    deterministic=self._trajectory_generator_config.deterministic,
-                )
-                obs_next, reward, done, __ = self._env.step(action)
-                if self._trajectory_generator_config.render:
-                    self._env.render()
-
-                snapshot_curr_step = np.hstack(
-                    [np.squeeze(obs_next), np.squeeze(action), reward, done]
-                )
-                self._trajectory.append(snapshot_curr_step)
-                snapshots_curr_episode.append(snapshot_curr_step)
-
-                reward_curr_episode += reward
-                self._num_steps += 1
-                obs_curr = obs_next
-
-            print("Episode reward: ", reward_curr_episode)
+            self._append_one_trajectory()
 
         self._env.close()
 
