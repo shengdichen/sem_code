2937000a87310c69a1d880b200aae8379ad588b6
shc <sh@c>
2023-01-14 00:34:09 +0100

    Add alg as member for sb3-manager

--- src/ours/util/expert/sb3/manager.py
+++ src/ours/util/expert/sb3/manager.py
@@ -3,6 +3,7 @@ from stable_baselines3.common.base_class import BaseAlgorithm
 
 from src.ours.util.common.param import CommonParam
 from src.ours.util.common.pathprovider import Sb3SaveLoadPathGenerator
+from src.ours.util.expert.sb3.util.model import AlgorithmFactory
 from src.ours.util.expert.sb3.util.saveload import Sb3Saver, Sb3Loader
 from src.ours.util.expert.sb3.util.train import Sb3Trainer
 
@@ -12,6 +13,7 @@ class Sb3Manager:
         self, env_and_identifier: tuple[gym.Env, str], training_param: CommonParam
     ):
         env, env_identifier = env_and_identifier
+        self._algorithm = AlgorithmFactory(env, training_param).get_algorithm()
         self._trainer = Sb3Trainer(env, training_param)
         self._path_saveload = Sb3SaveLoadPathGenerator(training_param).get_path(
             env_identifier
