5ef6a88515c5135c8e597aa1f3ebfb641c938ef9
shc <sh@c>
2023-01-13 21:23:07 +0100

    Prefix path with "zip" for sb3

--- src/ours/util/expert/sb3/util/saveload.py
+++ src/ours/util/expert/sb3/util/saveload.py
@@ -8,6 +8,12 @@ class Sb3Saver:
     def __init__(self, model: BaseAlgorithm, savepath_rel: Path):
         self._model = model
         self._savepath_rel = str(savepath_rel)
+        self._make_path_sb3_usable()
+
+    def _make_path_sb3_usable(self) -> None:
+        actual_suffix, image_suffix = self._savepath_rel[-4:], ".zip"
+        if actual_suffix != image_suffix:
+            self._savepath_rel += image_suffix
 
     def save_model(self):
         self._model.save(self._savepath_rel)
@@ -17,6 +23,12 @@ class Sb3Loader:
     def __init__(self, alg: BaseAlgorithm, savepath_rel: Path):
         self._alg = alg
         self._savepath_rel = str(savepath_rel)
+        self._make_path_sb3_usable()
+
+    def _make_path_sb3_usable(self) -> None:
+        actual_suffix, image_suffix = self._savepath_rel[-4:], ".zip"
+        if actual_suffix != image_suffix:
+            self._savepath_rel += image_suffix
 
     def load_model(self, new_env: gym.Env = None) -> BaseAlgorithm:
         return self._alg.load(self._savepath_rel, new_env)
