8dea3c47347ed2be70806fb53803af35ef0245d0
shc <sh@c>
2023-01-13 23:54:59 +0100

    Relocate model-creation to separate factory

---
+++ src/ours/util/expert/sb3/util/model.py
@@ -0,0 +1,20 @@
+from gym import Env
+from stable_baselines3 import PPO as PPOSB
+from stable_baselines3.common.base_class import BaseAlgorithm
+
+from src.ours.util.common.param import CommonParam
+
+
+class ModelFactory:
+    def __init__(self, env: Env, training_param: CommonParam):
+        self._env = env
+        self._training_param = training_param
+
+    def get_model(self) -> BaseAlgorithm:
+        return PPOSB(
+            "MlpPolicy",
+            self._env,
+            verbose=0,
+            **self._training_param.kwargs_ppo,
+            tensorboard_log=self._training_param.sb3_tblog_dir
+        )
