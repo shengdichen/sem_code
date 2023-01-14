e8efca51c736e2f4399d58d8decadcfcf5640e21
shc <sh@c>
2023-01-14 21:25:48 +0100

    Format single-plot with Black

--- src/ours/util/expert/trajectory/analyzer/plot/single.py
+++ src/ours/util/expert/trajectory/analyzer/plot/single.py
@@ -52,17 +52,13 @@ class TrajectoryPlot:
     def plot_agent_and_target(self, plot_agent_as_hist: bool) -> None:
         axs = self._figure.subplots(1, 2)
 
-        TrajectoryPlotAtom(self._trajectory, axs[0]).plot_agent(
-            plot_agent_as_hist
-        )
+        TrajectoryPlotAtom(self._trajectory, axs[0]).plot_agent(plot_agent_as_hist)
         TrajectoryPlotAtom(self._trajectory, axs[1]).plot_target()
 
     def plot_agent_and_action(self, plot_agent_as_hist: bool) -> None:
         axs = self._figure.subplots(1, 2)
 
-        TrajectoryPlotAtom(self._trajectory, axs[0]).plot_agent(
-            plot_agent_as_hist
-        )
+        TrajectoryPlotAtom(self._trajectory, axs[0]).plot_agent(plot_agent_as_hist)
         TrajectoryPlotAtom(self._trajectory, axs[1]).plot_action()
 
     def plot_agent_target_action(self) -> None:
