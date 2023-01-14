85b3f1593fa4f50ae1efa142caa041c5aea975e4
shc <sh@c>
2023-01-14 02:22:31 +0100

    Remove separate traj&sb3 path-gen'tor

--- src/ours/util/common/pathprovider.py
+++ src/ours/util/common/pathprovider.py
@@ -28,38 +28,6 @@ class ExpertSaveLoadPathGenerator:
         )
 
 
-class TrajectorySaveLoadPathGenerator:
-    def __init__(self, training_param: CommonParam):
-        self._training_param = training_param
-        self._np_postfix = ".npy"
-
-    def get_path(self, env_identifier: str) -> Path:
-        return Path(
-            "{0}/{1}{2}{3:07}{4}".format(
-                self._training_param.demo_dir,
-                env_identifier,
-                "_",
-                self._training_param.n_steps_expert_train,
-                self._np_postfix,
-            )
-        )
-
-
-class Sb3SaveLoadPathGenerator:
-    def __init__(self, training_param: CommonParam):
-        self._training_param = training_param
-
-    def get_path(self, env_identifier: str) -> Path:
-        return Path(
-            "{0}/{1}{2}{3:07}".format(
-                self._training_param.model_dir,
-                env_identifier,
-                "_",
-                self._training_param.n_steps_expert_train,
-            )
-        )
-
-
 class PwilSaveLoadPathGenerator:
     def __init__(self, training_param: PwilParam):
         self._training_param = training_param
