380ddd40899ff0e2c0ee5863b41f9c52d6649b38
shc <sh@c>
2023-01-14 02:21:03 +0100

    Use new path-gen'tor for traj-saveload

--- src/ours/util/expert/trajectory/manager.py
+++ src/ours/util/expert/trajectory/manager.py
@@ -4,7 +4,7 @@ import numpy as np
 from gym import Env
 
 from src.ours.util.common.param import CommonParam
-from src.ours.util.common.pathprovider import TrajectorySaveLoadPathGenerator
+from src.ours.util.common.pathprovider import ExpertSaveLoadPathGenerator
 from src.ours.util.expert.trajectory.analyzer.general import (
     TrajectoriesAnalyzerParallel,
 )
@@ -27,9 +27,9 @@ class TrajectoryManager:
         self._trajectory_generator = TrajectoryGenerator(
             (env, model), trajectory_generator_config
         )
-        self._path_saveload = TrajectorySaveLoadPathGenerator(training_param).get_path(
-            env_identifier
-        )
+        self._path_saveload = ExpertSaveLoadPathGenerator(
+            training_param
+        ).get_trajectory_path(env_identifier)
 
     def save(self) -> None:
         trajectory = self._trajectory_generator.get_trajectories()
