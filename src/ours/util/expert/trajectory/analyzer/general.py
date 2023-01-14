dbc5a4d5d1db1ee10b3fca52827d8fc8a96e579a
shc <sh@c>
2023-01-14 15:27:04 +0100

    Add separate stats-class for multi-trajs

--- src/ours/util/expert/trajectory/analyzer/general.py
+++ src/ours/util/expert/trajectory/analyzer/general.py
@@ -8,6 +8,19 @@ from src.ours.util.expert.trajectory.analyzer.stats import TrajectoryStats
 from src.ours.util.expert.trajectory.analyzer.util import MplUtil
 
 
+class TrajectoriesStats:
+    def __init__(self, trajectories: list[np.ndarray]):
+        self._trajectories = trajectories
+
+        self._trajectories_stats = [
+            TrajectoryStats(trajectory) for trajectory in self._trajectories
+        ]
+
+    def show_stats(self) -> None:
+        for trajectory_stats in self._trajectories_stats:
+            print(trajectory_stats.get_stats())
+
+
 class TrajectoriesAnalyzerBase(ABC):
     def __init__(self, trajectories: list[np.ndarray]):
         self._trajectories = trajectories
