f0dfa0d3d3d82c941ccfe5a256ac1d3dd6220b4c
shc <sh@c>
2023-01-16 17:14:57 +0100

    Add miss'g return type-hints

--- src/ours/env/run.py
+++ src/ours/env/run.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy as np
+
 from src.ours.env.env import MovePoint
 
 
@@ -10,20 +12,20 @@ class PointEnvRunner:
 
         self._obs, self._done = self.reset()
 
-    def reset(self):
+    def reset(self) -> tuple[np.ndarray, bool]:
         obs = self._env.reset()
         self._env.render("human")
         return obs, False
 
-    def close(self):
+    def close(self) -> None:
         self._env.close()
 
-    def take_action_random(self):
+    def take_action_random(self) -> None:
         for __ in range(self._n_steps):
             self._env.step(random.randint(0, 4))
             self._env.render()
 
-    def run_random(self, n_runs: int = 1):
+    def run_random(self, n_runs: int = 1) -> None:
         for __ in range(n_runs):
             self.reset()
             self.take_action_random()
