791fe79ab6f9e6f67a8d05499afb9bbf4865d7a7
shc <sh@c>
2023-01-19 02:23:30 +0100

    Add pwil model-creator with model-path as tb-log

--- src/ours/util/expert/sb3/util/model.py
+++ src/ours/util/expert/sb3/util/model.py
@@ -2,7 +2,8 @@ from gym import Env
 from stable_baselines3 import PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 
-from src.ours.util.common.param import CommonParam
+from src.ours.util.common.param import CommonParam, PwilParam
+from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
 
 
 class AlgorithmFactory:
@@ -17,4 +18,24 @@ class AlgorithmFactory:
             verbose=0,
             **self._training_param.kwargs_ppo,
             tensorboard_log=self._training_param.sb3_tblog_dir
+        )
+
+
+class AlgorithPwilFactory:
+    def __init__(self, env_and_identifier: tuple[Env, str], training_param: PwilParam):
+        self._env, self._env_identifier = env_and_identifier
+        self._training_param = training_param
+
+    def get_algorithm(self) -> BaseAlgorithm:
+        model_path = PwilSaveLoadPathGenerator(self._training_param).get_model_path(
+            self._env_identifier
+        )
+        model_dir = str(model_path) + "/log/"
+
+        return PPO(
+            "MlpPolicy",
+            self._env,
+            verbose=0,
+            **self._training_param.kwargs_ppo,
+            tensorboard_log=model_dir
         )
