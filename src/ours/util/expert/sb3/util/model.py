1900cebe53b5f5ef705e8283e59817b364fdaf24
shc <sh@c>
2023-01-19 21:04:05 +0100

    Set tb-log-dir within model-dir

--- src/ours/util/expert/sb3/util/model.py
+++ src/ours/util/expert/sb3/util/model.py
@@ -3,7 +3,10 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 
 from src.ours.util.common.param import CommonParam, PwilParam
-from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
+from src.ours.util.common.pathprovider import (
+    PwilSaveLoadPathGenerator,
+    ExpertSaveLoadPathGenerator,
+)
 
 
 class AlgorithmFactory:
@@ -14,12 +17,17 @@ class AlgorithmFactory:
         self._training_param = training_param
 
     def get_algorithm(self) -> BaseAlgorithm:
+        model_path = ExpertSaveLoadPathGenerator(
+            self._env_identifier, self._training_param
+        ).get_model_path()
+        model_dir = str(model_path) + "/log/"
+
         return PPO(
             "MlpPolicy",
             self._env,
             verbose=0,
             **self._training_param.kwargs_ppo,
-            tensorboard_log=self._training_param.sb3_tblog_dir
+            tensorboard_log=model_dir
         )
 
 
