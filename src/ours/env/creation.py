47aea625d546b8e09752efb1e94755092b9b77a9
shc <sh@c>
2023-01-20 08:17:19 +0100

    Copy for pointenv-identifier gen'tor-base

--- src/ours/env/creation.py
+++ src/ours/env/creation.py
@@ -30,6 +30,25 @@ class PointEnvConfigFactory:
         return self._env_configs
 
 
+class PointEnvIdentifierGeneratorBase:
+    def __init__(self, prefix: str):
+        self._prefix = prefix
+        self._connector = "_"
+
+    def get_identifier(self, env_config: dict[str:int]) -> str:
+        shift_x, shift_y = env_config["shift_x"], env_config["shift_y"]
+        return (
+            self._prefix
+            + self._connector
+            + "{0:03}".format(shift_x)
+            + self._connector
+            + "{0:03}".format(shift_y)
+        )
+
+    def from_env(self, env: MovePoint) -> str:
+        return self.get_identifier(env.env_config)
+
+
 class PointEnvIdentifierGenerator:
     def __init__(self):
         self._prefix = "pointenv"
