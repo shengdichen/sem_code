6ae95fb470018250ba8ff57b7ec4bf317978768b
shc <sh@c>
2023-01-20 09:12:14 +0100

    Implement obs-space in space-gen'r base

--- src/ours/env/util/space.py
+++ src/ours/env/util/space.py
@@ -3,14 +3,20 @@ from gym import spaces
 
 
 class SpaceGeneratorBase:
-    def __init__(self):
-        pass
+    def __init__(self, side_length: int):
+        self._side_length = side_length
 
     def get_spaces(self) -> tuple[spaces.Space, spaces.Space]:
         return self._get_observation_space(), self._get_action_space()
 
     def _get_observation_space(self) -> spaces.Space:
-        pass
+        n_movements_to_observe = 4
+
+        return spaces.Box(
+            low=np.zeros(n_movements_to_observe, dtype=np.float64),
+            high=np.ones(n_movements_to_observe, dtype=np.float64) * self._side_length,
+            dtype=np.float64,
+        )
 
     def _get_action_space(self) -> spaces.Space:
         pass
