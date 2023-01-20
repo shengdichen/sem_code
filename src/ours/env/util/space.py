a170d288f35f6cbf20a779de9d5d1ab1a57d289c
shc <sh@c>
2023-01-20 09:13:04 +0100

    Make discrete space-gen'r sub-class

--- src/ours/env/util/space.py
+++ src/ours/env/util/space.py
@@ -22,24 +22,11 @@ class SpaceGeneratorBase:
         pass
 
 
-class SpacesGenerator:
+class SpacesGenerator(SpaceGeneratorBase):
     def __init__(self, side_length: int):
-        self._side_length = side_length
+        super().__init__(side_length)
 
-    def get_spaces(self):
-        return self._get_observation_space(), self._get_action_space()
-
-    def _get_observation_space(self):
-        n_movements_to_observe = 4
-
-        return spaces.Box(
-            low=np.zeros(n_movements_to_observe, dtype=np.float64),
-            high=np.ones(n_movements_to_observe, dtype=np.float64) * self._side_length,
-            dtype=np.float64,
-        )
-
-    @staticmethod
-    def _get_action_space():
+    def _get_action_space(self) -> spaces.Space:
         n_legal_actions = 5
         return spaces.Discrete(n_legal_actions)
 
