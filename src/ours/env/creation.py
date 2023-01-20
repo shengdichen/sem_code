a4fdf2bf4f765c65d94b24ae7c21d4d7b93a5667
shc <sh@c>
2023-01-20 10:19:27 +0100

    Add pointenv-factory base

--- src/ours/env/creation.py
+++ src/ours/env/creation.py
@@ -9,6 +9,14 @@ class EnvFactory(ABC):
         pass
 
 
+class PointEnvFactoryBase(EnvFactory):
+    def __init__(self, env_config: dict[str:int]):
+        self._env_config = env_config
+
+    def create(self):
+        pass
+
+
 class PointEnvFactory(EnvFactory):
     def __init__(self, env_config: dict[str:int]):
         self._env_config = env_config
