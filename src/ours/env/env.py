6424b4a920617b46e2f8c87edf03fe535547bcec
shc <sh@c>
2023-01-20 10:09:59 +0100

    Add discrete & cont sub-class of pointenv

--- src/ours/env/env.py
+++ src/ours/env/env.py
@@ -6,7 +6,12 @@ from src.ours.env.component.visualizer import (
     PositionVisualizer,
 )
 from src.ours.env.component.field import Field
-from src.ours.env.util.space import SpacesGenerator, ActionConverter
+from src.ours.env.util.space import (
+    SpacesGenerator,
+    SpacesGeneratorCont,
+    ActionConverter,
+    ActionConverterCont,
+)
 from src.ours.env.util.renderer import PointEnvRendererHuman, PointEnvRendererRgb
 from src.ours.env.util.time import EpisodeLengthTimer
 
@@ -81,73 +86,35 @@ class MovePointBase(Env):
         PointEnvRendererHuman.clean_up()
 
 
-class MovePoint(Env):
+class MovePoint(MovePointBase):
     def __init__(self, n_targets=2, shift_x=0, shift_y=0, random_spawn_agent=False):
-        super().__init__()
+        super().__init__(
+            n_targets,
+            shift_x,
+            shift_y,
+            random_spawn_agent,
+        )
 
-        self._side_length = 200
         self.observation_space, self.action_space = SpacesGenerator(
             self._side_length
         ).get_spaces()
 
-        self._board_shape = self._side_length, self._side_length
-        self._field = Field(n_targets, (shift_x, shift_y), random_spawn_agent)
-        self._position_visualizer = PositionVisualizer(self._field)
-        self._trajectory_heat_visualizer = TrajectoryHeatVisualizer(self._field)
+    def _get_action_converted(self, action: int | np.ndarray) -> tuple[int, int]:
+        return ActionConverter(action, self.action_space).get_action_converted()
 
-        self._episode_timer = EpisodeLengthTimer(1000)
 
-    @property
-    def env_config(self):
-        return self._field.config
+class MovePointCont(MovePointBase):
+    def __init__(self, n_targets=2, shift_x=0, shift_y=0, random_spawn_agent=False):
+        super().__init__(
+            n_targets,
+            shift_x,
+            shift_y,
+            random_spawn_agent,
+        )
 
-    def _draw_elements_on_canvas(self):
-        self._position_visualizer.visualize()
-        self._trajectory_heat_visualizer.visualize()
+        self.observation_space, self.action_space = SpacesGeneratorCont(
+            self._side_length
+        ).get_spaces()
 
-    def reset(self):
-        self._field.reset()
-
-        self._draw_elements_on_canvas()
-
-        self._episode_timer.reset()
-
-        return self._get_obs()
-
-    def step(self, action: int):
-        action_converted = ActionConverter(
-            action, self.action_space
-        ).get_action_converted()
-        reward, has_visited_all_targets = self._field.step(action_converted)
-
-        self._draw_elements_on_canvas()
-
-        obs = self._get_obs()
-
-        has_elapsed = self._episode_timer.advance()
-
-        done = has_visited_all_targets or has_elapsed
-        return obs, reward, done, {}
-
-    def _get_obs(self):
-        field_obs = self._field.get_pos_agent_target()
-        return field_obs
-
-    def render(self, mode="human") -> None:
-        assert mode in [
-            "human",
-            "rgb_array",
-        ], 'Invalid mode, must be either "human" or "rgb_array"'
-
-        if mode == "human":
-            renderer = PointEnvRendererHuman(
-                self._position_visualizer.colormat,
-                self._trajectory_heat_visualizer.colormat,
-            )
-        else:
-            renderer = PointEnvRendererRgb(self._position_visualizer.colormat)
-
-        renderer.render()
-
-    def close(self):
-        PointEnvRendererHuman.clean_up()
+    def _get_action_converted(self, action: int | np.ndarray) -> tuple[int, int]:
+        return ActionConverterCont(action, self.action_space).get_action_converted()
