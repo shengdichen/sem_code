9fd3cd88847c775d7b799943c44ab8260eb968d3
shc <sh@c>
2023-01-20 11:52:38 +0100

    Add cont random action-provider

--- src/ours/eval/pointenv/run/actionprovider.py
+++ src/ours/eval/pointenv/run/actionprovider.py
@@ -11,3 +11,8 @@ class ActionProvider:
 class ActionProviderRandom(ActionProvider):
     def get_action(self, obs: np.ndarray, **kwargs) -> int:
         return random.randint(0, 4)
+
+
+class ActionProviderRandomCont(ActionProvider):
+    def get_action(self, obs: np.ndarray, **kwargs) -> np.ndarray:
+        return np.array(np.random.uniform([-2.5, -2.5], [+2.5, +2.5]))
