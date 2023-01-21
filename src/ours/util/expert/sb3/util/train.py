06ef81cc28c1096818ec89b18af1e6172d5a4f51
shc <sh@c>
2023-01-21 22:27:13 +0100

    Remove dup from expert sub-class

--- src/ours/util/expert/sb3/util/train.py
+++ src/ours/util/expert/sb3/util/train.py
@@ -1,11 +1,8 @@
 from gym import Env
 from stable_baselines3.common.base_class import BaseAlgorithm
 
-from src.ours.util.common.helper import TqdmCallback
 from src.ours.util.common.param import CommonParam
-from src.ours.util.common.pathprovider import ExpertSaveLoadPathGenerator
 from src.ours.util.common.train import Trainer
-from src.ours.util.pwil.sb3.util import CallbackListFactory
 
 
 class Sb3Trainer(Trainer):
@@ -16,25 +13,3 @@ class Sb3Trainer(Trainer):
         env_raw_testing_and_identifier: tuple[Env, str] = None,
     ):
         super().__init__(model, training_param, env_raw_testing_and_identifier)
-
-        self._model = model
-        self._training_param = training_param
-        env_raw_testing, env_identifier = env_raw_testing_and_identifier
-
-        if env_raw_testing_and_identifier is not None:
-            self._callback_list = CallbackListFactory(
-                env_raw_testing,
-                ExpertSaveLoadPathGenerator(env_identifier, self._training_param),
-            ).callback_list
-        else:
-            self._callback_list = [TqdmCallback()]
-
-    @property
-    def model(self):
-        return self._model
-
-    def train(self) -> None:
-        self._model.learn(
-            total_timesteps=self._training_param.n_steps_training,
-            callback=self._callback_list,
-        )
