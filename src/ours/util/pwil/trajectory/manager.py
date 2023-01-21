a5c48e2c33203f07a136fefff03c1387f73177cf
shc <sh@c>
2023-01-21 16:27:53 +0100

    Remove dup methods in pwil-traj-mgr

--- src/ours/util/pwil/trajectory/manager.py
+++ src/ours/util/pwil/trajectory/manager.py
@@ -1,18 +1,13 @@
 from typing import Any
 
-import numpy as np
 from gym import Env
-from matplotlib import pyplot as plt
 
 from src.ours.util.common.param import PwilParam
 from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
-from src.ours.util.expert.trajectory.analyzer.plot.single import TrajectoryPlot
-from src.ours.util.expert.trajectory.analyzer.stats.single import TrajectoryStats
 from src.ours.util.expert.trajectory.manager import TrajectoryManagerBase
 from src.ours.util.expert.trajectory.util.generator import (
     TrajectoryGeneratorConfig,
 )
-from src.ours.util.expert.trajectory.util.saveload import TrajectorySaveLoad
 
 
 class TrajectoryManager(TrajectoryManagerBase):
@@ -31,24 +26,3 @@ class TrajectoryManager(TrajectoryManagerBase):
             PwilSaveLoadPathGenerator(env_identifier, training_param),
             trajectory_generator_config,
         )
-
-    def save(self) -> None:
-        trajectory = self._trajectory_generator.get_trajectories()
-
-        TrajectorySaveLoad(self._path_saveload).save(trajectory)
-
-    def load(self) -> np.ndarray:
-        return TrajectorySaveLoad(self._path_saveload).load()
-
-    def save_stats(self) -> None:
-        with open(self._path_saveload, "w") as f:
-            f.write(TrajectoryStats(self.load()).get_stats())
-
-    def save_plot(self) -> None:
-        figure = plt.figure(figsize=(15, 12), dpi=200)
-
-        TrajectoryPlot(self.load(), figure).plot_agent_target_action()
-
-        figure.savefig(self._path_saveload)
-
-        plt.close()
