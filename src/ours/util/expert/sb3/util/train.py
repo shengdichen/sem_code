76eb1bbce9afc2190e278c658218de11807a44f4
shc <sh@c>
2023-01-21 22:49:36 +0100

    Add path-gen'r to c'tor in expert&pwil sub-class

--- src/ours/util/expert/sb3/util/train.py
+++ src/ours/util/expert/sb3/util/train.py
@@ -2,6 +2,7 @@ from gym import Env
 from stable_baselines3.common.base_class import BaseAlgorithm
 
 from src.ours.util.common.param import CommonParam
+from src.ours.util.common.pathprovider import ExpertSaveLoadPathGenerator
 from src.ours.util.common.train import Trainer
 
 
@@ -12,4 +13,11 @@ class Sb3Trainer(Trainer):
         training_param: CommonParam,
         env_raw_testing_and_identifier: tuple[Env, str] = None,
     ):
-        super().__init__(model, training_param, env_raw_testing_and_identifier)
+        __, env_identifier = env_raw_testing_and_identifier
+
+        super().__init__(
+            model,
+            training_param,
+            ExpertSaveLoadPathGenerator(env_identifier, training_param),
+            env_raw_testing_and_identifier,
+        )
--- src/ours/util/pwil/sb3/train.py
+++ src/ours/util/pwil/sb3/train.py
@@ -14,7 +14,14 @@ class Sb3PwilTrainer(Trainer):
         training_param: PwilParam,
         env_raw_testing_and_identifier: tuple[Env, str],
     ):
-        super().__init__(model, training_param, env_raw_testing_and_identifier)
+        __, env_identifier = env_raw_testing_and_identifier
+
+        super().__init__(
+            model,
+            training_param,
+            PwilSaveLoadPathGenerator(env_identifier, training_param),
+            env_raw_testing_and_identifier,
+        )
 
         self._model = model
         self._training_param = training_param
