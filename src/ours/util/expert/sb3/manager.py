8c3030404bbc02b3ff87054397b731692365511b
shc <sh@c>
2023-01-21 17:16:58 +0100

    Copy (from expert) for base

--- src/ours/util/expert/sb3/manager.py
+++ src/ours/util/expert/sb3/manager.py
@@ -8,6 +8,49 @@ from src.ours.util.expert.sb3.util.saveload import Sb3Saver, Sb3Loader
 from src.ours.util.expert.sb3.util.train import Sb3Trainer
 
 
+class Sb3ManagerBase:
+    def __init__(
+        self,
+        envs_and_identifier: tuple[tuple[gym.Env, gym.Env], str],
+        training_param: CommonParam,
+    ):
+        (self._env, self._env_eval), self._env_identifier = envs_and_identifier
+        self._best_sb3_model_path = ExpertSaveLoadPathGenerator(
+            self._env_identifier, training_param
+        ).get_best_sb3_model_path()
+        self._latest_sb3_model_path = ExpertSaveLoadPathGenerator(
+            self._env_identifier, training_param
+        ).get_latest_sb3_model_path()
+        self._model = self._get_model(
+            AlgorithmFactory(
+                (self._env, self._env_identifier), training_param
+            ).get_algorithm()
+        )
+
+        self._training_param = training_param
+
+    def _get_model(self, algorithm: BaseAlgorithm) -> BaseAlgorithm:
+        sb3_loader = Sb3Loader(algorithm, self._best_sb3_model_path)
+        if sb3_loader.exists():
+            return sb3_loader.load(self._env)
+        else:
+            return algorithm
+
+    @property
+    def model(self) -> BaseAlgorithm:
+        return self._model
+
+    def train(self) -> None:
+        trainer = Sb3Trainer(
+            self._model, self._training_param, (self._env_eval, self._env_identifier)
+        )
+        trainer.train()
+
+    def save(self) -> None:
+        saver = Sb3Saver(self._model, self._latest_sb3_model_path)
+        saver.save()
+
+
 class Sb3Manager:
     def __init__(
         self,
