60104be5a1efc58980b427dcde2aea5f4c958d1a
shc <sh@c>
2023-01-22 00:33:44 +0100

    Remove dup methods in pwil-sb3-mgr

--- src/ours/util/pwil/sb3/manager.py
+++ src/ours/util/pwil/sb3/manager.py
@@ -1,12 +1,10 @@
 from gym import Env
-from stable_baselines3.common.base_class import BaseAlgorithm
 
 from src.ours.util.common.param import PwilParam
 from src.ours.util.common.pathprovider import PwilSaveLoadPathGenerator
 from src.ours.util.common.test import PolicyTester
 from src.ours.util.expert.sb3.manager import Sb3ManagerBase
 from src.ours.util.expert.sb3.util.model import AlgorithPwilFactory
-from src.ours.util.expert.sb3.util.saveload import Sb3Saver, Sb3Loader
 from src.ours.util.pwil.sb3.train import Sb3PwilTrainer
 
 
@@ -27,27 +25,12 @@ class Sb3PwilManager(Sb3ManagerBase):
             ).get_algorithm(),
         )
 
-    def _get_model(self, algorithm: BaseAlgorithm) -> BaseAlgorithm:
-        sb3_loader = Sb3Loader(algorithm, self._best_sb3_model_path)
-        if sb3_loader.exists():
-            return sb3_loader.load(self._env)
-        else:
-            return algorithm
-
-    @property
-    def model(self) -> BaseAlgorithm:
-        return self._model
-
     def train(self) -> None:
         trainer = Sb3PwilTrainer(
             self._model, self._training_param, (self._env_eval, self._env_identifier)
         )
 
         trainer.train()
-
-    def save(self) -> None:
-        saver = Sb3Saver(self._model, self._latest_sb3_model_path)
-        saver.save()
 
     def test(self) -> None:
         PolicyTester.test_policy(self._model)
