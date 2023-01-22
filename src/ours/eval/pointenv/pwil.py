6838f887e842ca60b2badca9315d742a2f7634e0
shc <sh@c>
2023-01-22 20:27:03 +0100

    Set demo&id in factory-c'tor directly

--- src/ours/eval/pointenv/pwil.py
+++ src/ours/eval/pointenv/pwil.py
@@ -17,7 +17,11 @@ from src.ours.util.pwil.manager import (
 
 
 class PointEnvPwilManagerFactory:
-    def __init__(self, training_param: PwilParam = PwilParam()):
+    def __init__(
+        self,
+        demonstration_and_id: tuple[list[np.ndarray], int],
+        training_param: PwilParam = PwilParam(),
+    ):
         self._training_param = training_param
 
         env_config = PointEnvConfigFactory().env_configs[0]
@@ -27,8 +31,8 @@ class PointEnvPwilManagerFactory:
         )
         self._env_identifier = PointEnvIdentifierGenerator().from_env(self._env_raw)
 
-        self._demos_all = self._get_all_demos()
-        self._demos_selected = self._demos_all[training_param.trajectory_num]
+        self._demos_selected, self._demonstration_id = demonstration_and_id
+        self._training_param.trajectory_num = self._demonstration_id
 
     @property
     def pwil_manager(self) -> PwilManager:
@@ -37,57 +41,6 @@ class PointEnvPwilManagerFactory:
             ((self._env_raw, self._env_eval), self._env_identifier),
             self._demos_selected,
         ).pwil_manager
-
-    @staticmethod
-    def _get_all_demos():
-        pointenv_expert_default = PointEnvExpertDefault()
-
-        trajectories = pointenv_expert_default.load_trajectories()
-
-        trajectory_0 = PointEnvPwilManagerFactory.make_selected_trajectories(
-            trajectories, [0]
-        )
-
-        (trajectory_01, trajectory_02, trajectory_012) = (
-            PointEnvPwilManagerFactory.make_selected_trajectories(trajectories, [0, 1]),
-            PointEnvPwilManagerFactory.make_selected_trajectories(trajectories, [0, 2]),
-            PointEnvPwilManagerFactory.make_all_tractories(trajectories),
-        )
-
-        trajectory_1, trajectory_2, trajectory_12 = (
-            PointEnvPwilManagerFactory.make_selected_trajectories(trajectories, [1]),
-            PointEnvPwilManagerFactory.make_selected_trajectories(trajectories, [2]),
-            PointEnvPwilManagerFactory.make_selected_trajectories(trajectories, [1, 2]),
-        )
-
-        return (
-            trajectory_0,
-            trajectory_01,
-            trajectory_02,
-            trajectory_012,
-            trajectory_1,
-            trajectory_2,
-            trajectory_12,
-        )
-
-    @staticmethod
-    def make_selected_trajectories(
-        trajectories: list[np.ndarray],
-        selected_indexes: list[int],
-    ) -> list[np.ndarray]:
-        selected_trajectories = []
-        for index in selected_indexes:
-            selected_trajectories.extend(trajectories[index])
-
-        return selected_trajectories
-
-    @staticmethod
-    def make_all_tractories(trajectories: list[np.ndarray]) -> list[np.ndarray]:
-        selected_trajectories = []
-        for trajectory in trajectories:
-            selected_trajectories.extend(trajectory)
-
-        return selected_trajectories
 
     def set_pwil_training_param(
         self, n_demos: int = None, subsampling: int = None, use_actions: bool = None
@@ -102,15 +55,6 @@ class PointEnvPwilManagerFactory:
 
         if use_actions is not None:
             pwil_training_param["use_actions"] = use_actions
-
-        return self
-
-    def set_trajectories(
-        self, trajectories_num: int = None
-    ) -> "PointEnvPwilManagerFactory":
-        if trajectories_num is not None:
-            self._demos_selected = self._demos_all[trajectories_num]
-            self._training_param.trajectory_num = trajectories_num
 
         return self
 
