97eb0ea6424a4161ed818a6d0a540f4a22b8417e
shc <sh@c>
2023-01-24 11:08:15 +0100

    Add test-class for cont-pointenv

--- tests/env/env.py
+++ tests/env/env.py
@@ -12,3 +12,19 @@ class TestEnv:
         from src.ours.eval.pointenv.run.actionprovider import ActionProviderRandom
 
         self.get_runner().run_episodes(ActionProviderRandom())
+
+
+class TestContEnv:
+    @staticmethod
+    def get_runner():
+        from src.ours.eval.pointenv.run.run import PointEnvContRunner
+
+        return PointEnvContRunner()
+
+    def test_reset(self):
+        self.get_runner().reset()
+
+    def test_run_random(self):
+        from src.ours.eval.pointenv.run.actionprovider import ActionProviderRandomCont
+
+        self.get_runner().run_episodes(ActionProviderRandomCont())
