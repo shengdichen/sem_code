f803acc5a0ce6f149498ae40d9a4d655694c0602
shc <sh@c>
2023-01-24 11:02:11 +0100

    Remove test for clos'g env

--- tests/env/env.py
+++ tests/env/env.py
@@ -8,8 +8,5 @@ class TestEnv:
     def test_reset(self):
         self.get_runner().reset()
 
-    def test_close(self):
-        self.get_runner().close()
-
     def test_run_random(self):
         self.get_runner().run_random()
