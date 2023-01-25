2622d7f6dd8d640db55afae2724b7f256dce4aeb
shc <sh@c>
2023-01-25 08:09:01 +0100

    Move base sb3-alg-factory to own module

--- src/ours/util/expert/sb3/util/model.py
+++ src/ours/util/expert/sb3/util/model.py
@@ -1,35 +1,10 @@
 from gym import Env
-from stable_baselines3 import PPO
-from stable_baselines3.common.base_class import BaseAlgorithm
 
 from src.ours.util.common.param import CommonParam
+from src.ours.util.common.sb3.algorithm import AlgorithmFactory
 from src.ours.util.pwil.param import PwilParam
 from src.ours.util.pwil.path import PwilSaveLoadPathGenerator
 from src.ours.util.expert.path import ExpertSaveLoadPathGenerator
-from src.ours.util.common.saveload.path import SaveLoadPathGenerator
-
-
-class AlgorithmFactory:
-    def __init__(
-        self,
-        env: Env,
-        training_param: CommonParam,
-        saveload_path_generator: SaveLoadPathGenerator,
-    ):
-        self._env = env
-        self._training_param = training_param
-        self._tensorboard_log_dir = str(
-            saveload_path_generator.get_model_log_path(False)
-        )
-
-    def get_algorithm(self) -> BaseAlgorithm:
-        return PPO(
-            "MlpPolicy",
-            self._env,
-            verbose=0,
-            **self._training_param.kwargs_ppo,
-            tensorboard_log=self._tensorboard_log_dir
-        )
 
 
 class ExpertAlgorithmFactory(AlgorithmFactory):
