0083359b97ac1213aa0595c953676af5bea59bf8
shc <sh@c>
2023-01-26 06:19:29 +0100

    Add rewards&lengths get'r in trajS(!)-stats

--- src/ours/rl/common/trajectory/analyzer/stats/multi.py
+++ src/ours/rl/common/trajectory/analyzer/stats/multi.py
@@ -14,3 +14,39 @@ class TrajectoriesStats:
     def show_stats(self) -> None:
         for trajectory_stats in self._trajectories_stats:
             print(trajectory_stats.get_stats())
+
+    @property
+    def rewards_avg(self):
+        return np.array(
+            [
+                trajectory_stats.rewards_avg
+                for trajectory_stats in self._trajectories_stats
+            ]
+        )
+
+    @property
+    def rewards_std(self):
+        return np.array(
+            [
+                trajectory_stats.rewards_std
+                for trajectory_stats in self._trajectories_stats
+            ]
+        )
+
+    @property
+    def lengths_avg(self):
+        return np.array(
+            [
+                trajectory_stats.lengths_avg
+                for trajectory_stats in self._trajectories_stats
+            ]
+        )
+
+    @property
+    def lengths_std(self):
+        return np.array(
+            [
+                trajectory_stats.lengths_std
+                for trajectory_stats in self._trajectories_stats
+            ]
+        )
