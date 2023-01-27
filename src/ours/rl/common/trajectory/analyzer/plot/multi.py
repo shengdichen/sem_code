01aeae550c79f2b29d93bd9d9f9c0c7d5d0f53e4
shc <sh@c>
2023-01-27 16:23:07 +0100

    Use axhline for line across entire horizon

--- src/ours/rl/common/trajectory/analyzer/plot/multi.py
+++ src/ours/rl/common/trajectory/analyzer/plot/multi.py
@@ -244,8 +244,8 @@ class TrajectoriesComparisonPlot:
         ax.set_xlabel("Subsampling Frequency")
         ax.set_ylim([0, 1200])
 
-        ax.hlines(950, 0, 20)  # baseline
-        ax.hlines(250, 0, 20)  # expert
+        ax.axhline(950)  # baseline
+        ax.axhline(250)  # expert
 
         if stats_variant == "rewards_avg":
             ax.set_ylabel("Reward (higher is better)")
